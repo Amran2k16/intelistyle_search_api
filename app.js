07b00a3a61c4ae4bd4bba6024594529c1e8aa628
amran2k16 <amran4hmed@gmail.com>
2019-10-08 17:42:00 +0100

    almost done with main

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://studygoose.amran4hmed.now.sh"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // console.log(result.length);
 
